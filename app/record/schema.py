addec67710fe3588270c30895a260f5ed5b1fae7
Samuel Dantas <samuel.dantas@alasco.de>
2022-04-11 01:33:36 +0200

    Add record schema

---
+++ app/record/schema.py
@@ -0,0 +1,27 @@
+from dataclasses import dataclass, asdict
+
+
+@dataclass
+class Record:
+    id: int
+    qtype: str
+    qname: str
+    content: str
+
+    @classmethod
+    def from_instance(cls, instance):
+        return cls(id=instance.id,
+                   qtype=instance.qtype,
+                   qname=instance.qname,
+                   content=instance.content)
+
+
+class RecordsSchema:
+    def __init__(self, records):
+        if not isinstance(records, list):
+            self.records = [records]
+        else:
+            self.records = records
+
+    def serialize(self):
+        return [asdict(Record.from_instance(r)) for r in self.records]
---
+++ app/record/test_schema.py
@@ -0,0 +1,43 @@
+import pytest
+
+from fixtures import db
+
+from .schema import RecordsSchema
+from .model import Record
+
+
+@pytest.fixture
+def setup(db):
+    record1 = Record(qtype="A", qname="test.com", content="192.168.0.3")
+    record2 = Record(qtype="A", qname="2test.com", content="192.168.0.4")
+    db.session.add(record1)
+    db.session.add(record2)
+    db.session.commit()
+
+    yield Record.query.all()
+
+
+def test_records_schema_serialize_a_single_record(setup):
+    records = setup
+
+    records_schema = RecordsSchema(records[0])
+    serialized_records = records_schema.serialize()
+
+    for record, srecord in zip(records, serialized_records):
+        assert record.id == srecord['id']
+        assert record.qname == srecord['qname']
+        assert record.qtype == srecord['qtype']
+        assert record.content == srecord['content']
+
+
+def test_records_schema_serialize_multiple_records(setup):
+    records = setup
+
+    records_schema = RecordsSchema(records)
+    serialized_records = records_schema.serialize()
+
+    for record, srecord in zip(records, serialized_records):
+        assert record.id == srecord['id']
+        assert record.qname == srecord['qname']
+        assert record.qtype == srecord['qtype']
+        assert record.content == srecord['content']
