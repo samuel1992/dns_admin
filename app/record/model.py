8705d90c8a06e6987bb272917bf9c388b90dbe06
Samuel Dantas <samuel.dantas@alasco.de>
2022-04-11 00:58:19 +0200

    Ignore env folder

---
+++ app/record/model.py
@@ -0,0 +1,10 @@
+from extensions import db
+
+
+class Record(db.Model):
+    __tablename__ = 'records'
+
+    id = db.Column(db.Integer(), primary_key=True)
+    qtype = db.Column(db.String())
+    qname = db.Column(db.String())
+    content = db.Column(db.String())
